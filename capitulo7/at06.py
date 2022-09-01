a3dbeff9170c619f6a3ef7de5e07e08855d13c0d
rennisson <rennisson.alves@gmail.com>
2022-09-01 14:49:08 -0300

    Adição da Atividade 06

---
+++ capitulo7/at06.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+'''
+    Atividade 06 - classe Horario
+
+    Indique o código do Grupo (deveria ser o mesmo da computador
+    que você usou no CEC, como A, B, C etc.).
+
+    GRUPO: A
+
+    Liste a seguir os nomes completos dos membros do time que participaram dessa atividade presencialmente:
+
+    - Rennisson Davi D. Alves
+    - Caio Domiciano Pires dos Santos
+    - Gabriel Rodrigues Franchin
+
+'''
+
+
+def main():
+    ''' Testes da classe Horario
+    '''
+    print("Testes da classe Horario\n")
+    print("Testes da classe Horario\n")
+
+    t1 = Horario(8)
+    print(f't1 = {t1} e deve ser 08:00:00')
+
+    t2 = Horario(1, 40)
+    print(f't2 = {t2} e deve ser 01:40:00')
+
+    h1 = Horario(0, 40)
+    h2 = Horario(0, 0, 30)
+    h3 = Horario(50)
+    print(h1, h2, h3)
+    hm = h_min([h1, h2, h3])
+    print(hm)
+
+    t123 = Horario(1, 2, 3)
+    print(f't123 = {t123} e deve ser 01:02:03')
+
+    ti = t123 + 2
+    print(f'ti = {ti} e deve ser 03:02:03')
+
+    ti = ti - 1
+    print(f'ti = {ti} e deve ser 02:02:03')
+
+    ti = 10 - ti
+    print(f'ti = {ti} e deve ser 07:57:57')
+
+    tf = 2.5 + t123
+    print(f'tf = {tf} e deve ser 03:32:03')
+
+    tf = tf - 1.2345
+    print(f'tf = {tf} e deve ser 02:17:59')
+
+
+class Horario:
+    '''Classe utilizada para representar um horário.
+
+    Um horário é representado por três números inteiros maiores ou iguais
+    a zero, armazenados em um atributo do tipo lista e de nome 'dados'.
+
+       * `dados[2]`: um número inteiro entre 0 e 23 que indica horas
+       * `dados[1]`: um número inteiro entre 0 e 59 que indica minutos
+       * `dados[0]`: um número inteiro entre 0 e 59 que indica segundos
+
+    Essa classe deve permitir os "comportamentos" ilustrados no enunciado.
+    '''
+
+    def __init__(self, hora=0, minutos=0, segundos=0):
+        ''' (int, int, int) -> None
+        Chamado pelo construtor da classe.
+
+        Recebe uma lista de dados que serão referência para segundos, minutos e horas
+
+        '''
+        self.dados = [0, 0, 0]
+
+        if hora > 23:
+            hora = hora % 24
+        if minutos > 59:
+            minutos = minutos % 60
+        if segundos > 59:
+            segundos = segundos % 60
+
+        self.dados[0] = segundos
+        self.dados[1] = minutos
+        self.dados[2] = hora
+        # print(self.dados)
+
+    def __str__(self):
+        return f'{self.dados[2]:02}:{self.dados[1]:02}:{self.dados[0]:02}'
+
+    def __add__(self, other):
+        '''
+        '''
+
+        if type(other) is int:
+            seg = self.dados[0]
+            minutos = self.dados[1]
+            hora = self.dados[2] + other
+        elif type(other) is float:
+            other = transf(other)
+            seg = self.dados[0] + other.dados[0]
+            minutos = self.dados[1] + other.dados[1]
+            hora = self.dados[2] + other.dados[2]
+        else:
+            seg = self.dados[0] + other.dados[0]
+            minutos = self.dados[1] + other.dados[1]
+            hora = self.dados[2] + other.dados[2]
+
+        if seg > 59:
+            minutos += seg // 60
+            seg = seg % 60
+        if minutos > 59:
+            hora += minutos // 60
+            minutos = minutos % 60
+
+        return Horario(hora, minutos, seg)
+
+    def __radd__(self, other):
+
+        return self + other
+
+    def __sub__(self, other):
+
+        if type(other) is int:
+            seg = self.dados[0]
+            minutos = self.dados[1]
+            hora = self.dados[2] - other
+        elif type(other) is float:
+            other = transf(other)
+            seg = self.dados[0] - other.dados[0]
+            minutos = self.dados[1] - other.dados[1]
+            hora = self.dados[2] - other.dados[2]
+        else:
+            seg = self.dados[0] - other.dados[0]
+            minutos = self.dados[1] - other.dados[1]
+            hora = self.dados[2] - other.dados[2]
+
+        if seg > 59:
+            minutos += seg // 60
+            seg = seg % 60
+        if minutos > 59:
+            hora += minutos // 60
+            minutos = minutos % 60
+
+        return Horario(hora, minutos, seg)
+
+    def __rsub__(self, other):
+        other = transf(other)
+        return self - other
+
+    def __lt__(self, other):
+        if self.dados[2] < other.dados[2]:
+            return True
+        else:
+            return False
+
+    # ------------------------------------------------------------------------------
+    # INSIRA OUTROS MÉTODOS DA CLASSE HORARIO AQUI
+    # ------------------------------------------------------------------------------
+
+    # ------------------------------------------------------------------------------
+    #     FIM DA CLASSE HORARIO
+    # ------------------------------------------------------------------------------
+
+
+# ===================================================================
+#  Insira as funções que desejar aqui. CUIDADO com a tabulação.
+# ===================================================================
+
+def h_min(seq):
+    '''(list) -> Horario
+    Recebe uma lista seq com um ou mais objetos do tipo Horario.
+    Re00:00:50torna uma referência para um objeto Horario de menor valor em seq.
+
+    Por exemplo:
+    >>> h1 = Horario(0,50)
+    >>> h2 = Horario(0,0,50)
+    >>> h3 = Horario(50)
+    >>> hm = h_min( [h1, h2, h3] )
+    >>> print(hm)
+    00:00:50
+    '''
+    h_menor = seq[0]
+
+    for i in range(1, len(seq)):
+        if seq[i] < h_menor:
+            h_menor = seq[i]
+
+    return h_menor
+
+
+def transf(x):
+    '''
+    Recebe um float e transforma em Horario
+    '''
+    hora = int(x // 1)
+    minuto = (int(x % 1) * 60)
+    segundo = 0
+
+    y = minuto
+
+    if minuto % 1 != 0:
+        minuto = int(minuto // 1)
+        segundo = int((y % 1) * 60)
+
+    return Horario(hora, minuto, segundo)
+
+
+####### ========================================================== #######
+#####
+##     NAO REMOVA OU MODIFIQUE AS LINHAS A SEGUIR QUE TERMINAM O ARQUIVO
+#####
+####### ========================================================== #######
+#  fim da definição de todas as funções e classes
+#  chama a main
+## =============================================================
+if __name__ == '__main__':
+    main()
+
